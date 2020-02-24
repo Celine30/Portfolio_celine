0bf64f73a223c508db89294654af02da2a11d68d
Celine <celi.caurier@gmail.com>
2020-02-24 15:29:44 +0100

    Send mail

--- src/controller/BackController.php
+++ src/controller/BackController.php
@@ -10,7 +10,7 @@ use Project\Model;
 class BackController
 {
 
-     protected $twig = null;
+    protected $twig = null;
 
     public function __construct(Environment $twig)
     {
@@ -18,10 +18,12 @@ class BackController
     }
 
 
-        public function contact()
+    public function contact()
     {
 
-        $to      = 'celi.caurier@gmail.com';
+        /*EN LOCAL
+
+        $to = 'celi.caurier@gmail.com';
         $subject = htmlspecialchars($_POST['sujet']);
         $corpsMail = htmlspecialchars($_POST['message']);
         $headers = array(
@@ -30,51 +32,44 @@ class BackController
             'X-Mailer' => 'PHP/' . phpversion()
         );
 
-       mail($to, $subject, $corpsMail, $headers);
+        mail($to, $subject, $corpsMail, $headers);
 
 
-            $message_ok = 'Message bien envoyé ! ';
-            return $this->twig->render('contact.twig', array(
-                'message' => $message_ok
-            ));
+        $message_ok = 'Message bien envoyé ! ';
+        return $this->twig->render('contact.twig', array(
+            'message' => $message_ok
+        ));
 
-        }
+        */
 
-
-/*SUR LE SERVEUR
-
-
-        $name       = @trim(stripslashes($_POST['nom']));
-        $fromBis    = @trim(stripslashes($_POST['mail']));
-        $from       = 'contact@gbaf.fr';
-        $subject    = @trim(stripslashes($_POST['sujet']));
-        $corps      = @trim(stripslashes($_POST['message']));
-        $to         = 'celine@izycom-communication.fr';
+        /*SUR LE SERVEUR*/
 
 
-        $headers  = "";
-        $headers .= "MIME-Version: 1.0\r\n";
-        $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
-        $headers .= "From: ContactGBAF <{$from}>\r\n";
-        $headers .= "Reply-To: <{$fromBis}>\r\n";
-        $headers .= "Subject: {$subject}\r\n";
-        $headers .= "X-Mailer: PHP/".phpversion()."\r\n";
+                $name       = @trim(stripslashes($_POST['nom']));
+                $fromBis    = @trim(stripslashes($_POST['mail']));
+                $from       = 'Portfolio.fr';
+                $subject    = @trim(stripslashes($_POST['sujet']));
+                $corps      = @trim(stripslashes($_POST['message']));
+                $to         = 'celi.caurier@gmail.com';
 
-        //mail($to, $subject, $corps, $headers);
 
-    echo($to);
-    echo("<br>");
-     echo($subject);
-    echo("<br>");
-  echo($corps);
-    echo("<br>");
- echo($headers);
+                $headers  = "";
+                $headers .= "MIME-Version: 1.0\r\n";
+                $headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
+                $headers .= "From: Portfolio <{$from}>\r\n";
+                $headers .= "Reply-To: <{$fromBis}>\r\n";
+                $headers .= "Subject: {$subject}\r\n";
+                $headers .= "X-Mailer: PHP/".phpversion()."\r\n";
 
-  echo($fromBis);
+                mail($to, $subject, $corps, $headers);
 
-*/
+                $message_ok = 'Message bien envoyé ! ';
+
+                return $this->twig->render('contact.twig', array(
+                    'message' => $message_ok
+                ));
 
 
 
-
+    }
 }
